aced89ff1913f510780e04bb486c62d0a74c0e65
NikiAlexs <nikilexs@gmail.com>
2024-11-20 14:11:15 +0700

    Add files via upload

---
+++ lesson 1_6.py
@@ -0,0 +1,14 @@
+name = 'NikiAlexs'
+print(name)
+namt = "Wolf"
+print(name)
+date_of_birth = "Feb 1991"
+dateOfBirth = "Feb 1991"
+
+# Практическое задание
+f_dz = 12
+h_used = 1.5
+name_k = "Python"
+time = h_used / f_dz
+print(time)
+print('Курс:',name_k,'всего задач:',f_dz,'затрачено часов:',h_used,'среднее время выполнения 0.125','часа')
